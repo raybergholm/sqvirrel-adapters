1d327421a2ac76daf91e41167cd875b106e38a99
Ray Bergholm <raybergholm@gmail.com>
2018-06-19 16:36:04 +0300

    add polyfill to index.js

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const nodeHttpsRequest = require("./src/nodeHttpsRequest");
 const superagent = require("./src/superagent");
 const xmlHttpRequest = require("./src/xmlHttpRequest");
 
+const polyfill = () => {
+    if (superagent) {
+        return superagent;
+    } else if (nodeHttpsRequest) {
+        return nodeHttpsRequest;
+    } else if (xmlHttpRequest) {
+        return xmlHttpRequest;
+    } else {
+        throw new Error("Could not instantiate any adapter!");
+    }
+};
+
 module.exports = {
     nodeHttpsRequest,
     superagent,
-    xmlHttpRequest
+    xmlHttpRequest,
+    polyfill
 };
